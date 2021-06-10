3d1927556fbe47763bf13ed8d1e4abb376eda88c
Johan Castillo <jcjohan2707@gmail.com>
2021-06-09 20:59:59 -0400

    Run server and database

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,60 @@
-from flask import Flask
+# Dependencies
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
-
+# Flask intance
 app = Flask(__name__)
 
+# Database parameter
+host = "127.0.0.1"         
+user = "someUser"          
+passwd = "somePassword"    
+database = "ucb-pvapp" 
+
+# Database MySQL settings
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://jcjohan:password@localhost/guakamayafood"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# ORM settings
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+# Data Modeling
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(70), unique = True)
+    description = db.Column(db.String(100))
+
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+
+# Create tables
+db.create_all()
+
+# Database Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description')
+
+# Instance Schema
+product_schema = ProductSchema()
+products_schema = ProductSchema(many = True)
+
+
+# Endpoints
 @app.route("/")
-def Home():
-    return "<h1>Hellooo World!</h1>"
+def home():
+    return "<h1>Welcome to my API REST!</h1>"
+
+@app.route("/api/products", methods = ['POST'])
+def create_product():
+    print(request.json)
+    return "Hello"
 
 
 app.run(port = 8080, debug = True, host = "0.0.0.0")
